ed2707c8d2276c533913511989359b88ef646ed1
vidvukobratovic <vukobratovicvid@gmail.com>
2023-11-18 20:26:48 -0500

    first commit, mostly boiler plate code

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>vid's blog</div>
+      <main>
+        <div class="nav-bar">
+          <a href>about</a>
+          <a href>blog</a>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
